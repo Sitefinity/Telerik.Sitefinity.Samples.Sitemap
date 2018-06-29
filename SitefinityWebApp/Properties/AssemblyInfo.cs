748a92238d2d753c3cb3c173dfabd25936465184
Ivan Dimitrov <ivan.dimitrov@telerik.com>
2018-06-29 17:09:44 +0300

    Update copyright
    Update copyright

--- SitefinityWebApp/Properties/AssemblyInfo.cs
+++ SitefinityWebApp/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Progress")]
 [assembly: AssemblyProduct("Progress Sitefinity CMS")]
-[assembly: AssemblyCopyright("© 2016 Telerik AD. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2005-2018 Telerik EAD. All rights reserved.")]
 [assembly: AssemblyTrademark("Sitefinity")]
 [assembly: AssemblyCulture("")]
 
